3a35f7b2362ca18cce3f9ada630773c1809ebd50
dbpieter db <dbpieter db>
2017-02-24 21:27:01 +0100

    ditch reflect

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,5 @@
 import 'core-js';
 import 'zone.js';
-import 'reflect-metadata';
 
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app.module';
